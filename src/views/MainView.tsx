8b8d8cc03c8b9a6d2d00c0d374a9d677ac8409a4
Vince Au <vinceau09@gmail.com>
2021-06-13 23:29:54 +1000

    fix: remove console.log

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -6,17 +6,17 @@ export const MainView: React.FC = () => {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const paramMap: Record<string, any> = {};
-  console.log(
-    Array.from(params.entries()).forEach(([key, value]) => {
-      let val: any = value;
-      if (value === "true") {
-        val = true;
-      } else if (value === "false") {
-        val = false;
-      }
-      paramMap[key] = val;
-    })
-  );
+
+  Array.from(params.entries()).forEach(([key, value]) => {
+    let val: any = value;
+    if (value === "true") {
+      val = true;
+    } else if (value === "false") {
+      val = false;
+    }
+    paramMap[key] = val;
+  });
+
   return (
     <TwitterTimelineEmbed
       sourceType="profile"
