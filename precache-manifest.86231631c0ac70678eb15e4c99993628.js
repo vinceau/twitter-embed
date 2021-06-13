ea9972ca02346d6ee5dc346d9fcfd6274ecb079b
vinceau <vinceau@users.noreply.github.com>
2021-06-13 12:58:13 +0000

    Automated deployment: Sun Jun 13 12:58:13 UTC 2021 411b7382b036a911bfe1643c4fbf8f43c20edde0

---
+++ precache-manifest.86231631c0ac70678eb15e4c99993628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b1e3451fe7f92bcc638b8a2069e677",
+    "url": "/twitter-embed/index.html"
+  },
+  {
+    "revision": "4c7bee34b99a67a90471",
+    "url": "/twitter-embed/static/css/main.bbf81423.chunk.css"
+  },
+  {
+    "revision": "534a5c88442fcf08d132",
+    "url": "/twitter-embed/static/js/2.c7c16473.chunk.js"
+  },
+  {
+    "revision": "718fa98a469a94012f350cb6e7e01f06",
+    "url": "/twitter-embed/static/js/2.c7c16473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7bee34b99a67a90471",
+    "url": "/twitter-embed/static/js/main.09d55e1b.chunk.js"
+  },
+  {
+    "revision": "a1a1acdaf19cf1944c66",
+    "url": "/twitter-embed/static/js/runtime-main.31fdefe0.js"
+  }
+]);
